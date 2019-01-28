9eadb4c4ea9a7308013f55af87a5f810363d0689
João Victor Barros <joaovictor107543210@hotmail.com>
2019-01-28 14:51:03 -0300

    exercício2
    descobrir o número proposto pelo usuário, se é par ou impar

---
+++ impar_ou_par.py
@@ -0,0 +1,6 @@
+n1=int(input('Digite o número desejado, que eu falarei se é par ou impar:'))
+if n1%2==0:
+  print('Esse número é par!')
+else:
+  print('Esse número é impar')
+print('Fim do programa')
