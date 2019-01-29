83f6571d752c9756ed1e26f3ca9aa299524dc414
João Victor Barros <joaovictor107543210@hotmail.com>
2019-01-29 18:06:11 -0300

    exercicio 8 gua

---
+++ conversão_de_medidas.py
@@ -0,0 +1,4 @@
+medida=float(input('Digite a medida que você quer que converta em centímetros e milímetros:'))
+cm=medida*100
+mm=medida*1000
+print('A medidade informada em metros foi: {:.0f} metros, e a conversão em centímetros foi: {:.0f} cm, e em milímetros foi: {:.0f} mm'.format(medida,cm,mm))
