fe6c1101c7609555472516109358d2a6b1cefd0a
João Victor Barros <joaovictor107543210@hotmail.com>
2019-01-29 18:00:12 -0300

    exercicio 7 gua

---
+++ Média_aritmética.py
@@ -0,0 +1,4 @@
+n1=float(input('Digite sua primeira nota:'))
+n2=float(input('Digite sua segunda nota:'))
+nota_final=((n1+n2)/2)
+print('A primeira nota informada foi: {:.1f}, a segunda nota informada foi: {:.1f}, e a sua média é : {:.1f}'.format(n1,n2,nota_final))
