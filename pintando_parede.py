c1efe955d4ed948922f066100cd7807c606d0938
João Victor Barros <joaovictor107543210@hotmail.com>
2019-01-29 21:26:36 -0300

    exercício 11 gua

---
+++ pintando_parede.py
@@ -0,0 +1,6 @@
+larg=float(input('Largura da parede:'))
+altu=float(input('Altura da parede:'))
+área=larg*altu
+print('Sua parede tem dimensão de {}x{} e sua área é de {}'.format(larg,altu,área))
+tinta=área/2
+print('Para pintar a parede,você precisará de {}Litros de tinta'.format(tinta))
